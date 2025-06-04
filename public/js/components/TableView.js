6d4ab3322d3eacf3910ecd5e5eed4272a9ae312b
Lucas Cervera <lucas@lucascervera.com>
2025-06-04 19:01:35 +0200

    Add FieldRenderer component and field metadata

--- public/js/components/TableView.js
+++ public/js/components/TableView.js
@@ -1,8 +1,12 @@
+import { FieldRenderer } from './FieldRenderer.js';
+import { getFieldMeta } from '../fieldMeta.js';
+
 export const TableView = {
     props: {
         items: { type: Array, default: () => [] },
         fields: { type: [Array, Object], default: () => [] }
     },
+    components: { FieldRenderer },
     emits: ['update-item'],
     data() {
         return {
@@ -29,9 +33,12 @@ export const TableView = {
     methods: {
         prepareFields(f) {
             if (Array.isArray(f)) {
-                this.mappedFields = f;
+                this.mappedFields = f.map(field => ({ ...field, icon: getFieldMeta(field.key).icon, tooltip: getFieldMeta(field.key).tooltip }));
             } else if (f && typeof f === 'object' && this.$root && this.$root.generateFormFields) {
-                this.mappedFields = this.$root.generateFormFields(f);
+                this.mappedFields = this.$root.generateFormFields(f).map(field => {
+                    const meta = getFieldMeta(field.key);
+                    return { ...field, icon: meta.icon, tooltip: meta.tooltip };
+                });
             } else {
                 this.mappedFields = [];
             }
@@ -59,20 +66,13 @@ export const TableView = {
                 <tbody class="bg-white divide-y divide-gray-200">
                     <tr v-for="(row, rowIndex) in editableItems" :key="rowIndex">
                         <td v-for="field in mappedFields" :key="field.key" class="px-3 py-2 whitespace-nowrap">
-                            <template v-if="field.enum">
-                                <select v-model="row[field.key]" @change="emitUpdate(rowIndex)" class="form-select w-full">
-                                    <option v-for="option in field.enum" :key="option" :value="option">{{ option }}</option>
-                                </select>
-                            </template>
-                            <template v-else-if="field.format === 'date'">
-                                <input type="date" v-model="row[field.key]" @input="emitUpdate(rowIndex)" class="form-input w-full"/>
-                            </template>
-                            <template v-else-if="field.type === 'number' || field.type === 'integer'">
-                                <input type="number" v-model.number="row[field.key]" @input="emitUpdate(rowIndex)" class="form-input w-full"/>
-                            </template>
-                            <template v-else>
-                                <input type="text" v-model="row[field.key]" @input="emitUpdate(rowIndex)" class="form-input w-full"/>
-                            </template>
+                            <field-renderer
+                                :field-key="field.key"
+                                :value="row[field.key]"
+                                :field-meta="field"
+                                editable
+                                @update="val => { row[field.key] = val; emitUpdate(rowIndex); }"
+                            ></field-renderer>
                         </td>
                     </tr>
                 </tbody>
